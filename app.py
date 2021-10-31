384ba073414e69b629f8d3602595586b20c71ad9
CJepps <cjepps@hotmail.co.uk>
2021-10-31 21:08:38 +0000

    Created add recipe template.

--- app.py
+++ app.py
@@ -108,6 +108,10 @@ def edit_recipes():
     return render_template("edit_recipes.html")
 
 
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
